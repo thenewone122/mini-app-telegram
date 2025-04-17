3ad7277ae9c73b9cb11efad3a7cb73d4216a3216
thenewone122 <swghhshe@gmail.com>
2025-04-17 17:21:30 +0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
+const eslintConfig = [...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off", // 👈 disable the rule here
+    },
+}];
 export default eslintConfig;
